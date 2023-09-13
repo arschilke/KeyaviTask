5dbb0c09a34cdb5313fa680187d04b0949122615
alyssaschilke <arschilke@loyola.edu>
2023-09-13 01:03:46 -0400

    read me and args

--- Program.cs
+++ Program.cs
@@ -8,18 +8,20 @@ namespace KeyaviTask
     {
         static void Main(string[] args)
         {
-            var input = new List<int>() { 4, 2, 8, 5, 8, 3, 1, 12, };
+            var input = new List<int>();
+            input.AddRange(args.Select(x => int.Parse(x)));
+
             int thirdMax = Program.ThirdLargest(input);
-            Console.WriteLine($"The Third Largest of {string.Join(",", input)} is {thirdMax}");
+            Console.WriteLine($"The third largest element of {string.Join(",", input)} is {thirdMax}");
         }
 
         /// <summary>
         /// This method will return the third largest number
         /// </summary>
-        /// <param name="numbers"></param>
-        public static int ThirdLargest(IEnumerable<int> numbers)
+        /// <param name="numbers">List of intergers</param>
+        public static int ThirdLargest(List<int> numbers)
         {
-            if (numbers.Count() < 3)
+            if (numbers.Count < 3)
             {
                 throw new InvalidOperationException("Collection must have at least 3 values");
             }
@@ -41,7 +43,6 @@ namespace KeyaviTask
                 }
                 else if (num > max3)
                     max3 = num;
-                Console.WriteLine($"max: {max}, max2: {max2}, max3: {max3}");
             }
             return max3;
         }
---
+++ README.md
@@ -0,0 +1,12 @@
+# Keyavi Coding Assessment
+
+## Usage
+
+```shell
+KeyaviTask.exe  4 2 8 5 8 3 1 12 #list of itergers delimited by a space
+```
+
+Using your strongest coding language, please send back a code method that returns the 3rd largest element in a list.
+
+Tips:
+You do not need to implement a sorting algorithm. We encourage you to consider other designs that might be more optimal than sorting, if any. Also, if there are any tradeoffs, please document that in your code.
